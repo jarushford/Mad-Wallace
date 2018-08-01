0f182e931c1039ac93ac7c8ed93205c4e682b2e8
Jamie Rushford <jar.rushford@gmail.com>
2018-08-01 17:46:56 -0600

    JS global

--- javascript/global.js
+++ javascript/global.js
@@ -1,5 +1,32 @@
-var suscribeButton = document.querySelector('#suscribe-button');
+function buttonToggle(x) {
+    x.classList.toggle("change");
+}
+	
+function menuToggle() {
+	var element = document.getElementById("collapsed");
+	element.classList.toggle("menu-content");
+	element.classList.toggle("open");
+}
+	
+function hover(element) {
+	element.setAttribute('src', 'images/about-head-alt.jpg');
+}
 
-suscribeButton.AddEventListener('click', function(){
-  document.getElementbyId("#email-input").reset();
+function unhover(element) {
+	element.setAttribute('src', 'images/about-head.jpg');
+}
+
+$(document).ready(function () {
+    $(".arrow-right").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "+=336"
+        }, 750);
+    });
+    $(".arrow-left").bind("click", function (event) {
+        event.preventDefault();
+        $(".vid-list-container").stop().animate({
+            scrollLeft: "-=336"
+        }, 750);
+    });
 });
